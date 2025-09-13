2ea0ce2e7ee7459e8517012949a5c37b1ffbfacd
Kent Dang <kenttdang@gmail.com>
2025-09-13 15:23:46 -0400

    i hate you guys (#4)

---
+++ packages/electron.js
@@ -0,0 +1,46 @@
+// electron.js
+const { app, BrowserWindow, systemPreferences } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webSecurity: false,
+      allowRunningInsecureContent: true,
+    },
+  });
+
+  // Request camera permissions on macOS
+  if (process.platform === 'darwin') {
+    systemPreferences.askForMediaAccess('camera');
+  }
+
+  const startURL = isDev
+    ? "http://localhost:3000"
+    : `file://${path.join(__dirname, "../build/index.html")}`;
+
+  mainWindow.loadURL(startURL);
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
