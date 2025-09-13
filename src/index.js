65f540a29900e223290bb213190ed6abc0187f4b
Kent Dang <kenttdang@gmail.com>
2025-09-13 12:30:33 -0400

    Dev (#2)
    * 🏆 feat: pose detection
    
    * oops added styling back

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-const { app, BrowserWindow } = require("electron");
+// main.js
+const { app, BrowserWindow, session } = require("electron");
 const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 960,
+    height: 720,
     webPreferences: {
-      contextIsolation: true,
       nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+      webSecurity: true,
+      allowRunningInsecureContent: false,
     },
   });
 
-  win.loadFile("src/rindex.html");
+  // Auto-allow webcam and other necessary permissions
+  session.defaultSession.setPermissionRequestHandler(
+    (webContents, permission, callback) => {
+      if (permission === "media" || permission === "camera") {
+        return callback(true);
+      }
+      callback(false);
+    }
+  );
+
+  // Allow loading external scripts for MediaPipe CDN
+  session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
+    callback({
+      responseHeaders: {
+        ...details.responseHeaders,
+        'Content-Security-Policy': ['script-src \'self\' \'unsafe-inline\' \'unsafe-eval\' https://cdn.jsdelivr.net; connect-src \'self\' https://cdn.jsdelivr.net;']
+      }
+    });
+  });
+
+  win.loadFile(path.join(__dirname, "index.html"));
 }
 
 app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
